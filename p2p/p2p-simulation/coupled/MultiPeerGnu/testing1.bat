55186f7e77814c973bfab46b2b2ee91c8fbae09c
DDionne <djdionne@connect.carleton.ca>
2011-08-19 16:10:37 -0400

    added file used to make the graph of frequency vs probability

--- p2p/p2p-simulation/coupled/MultiPeerGnu/testing1.bat
+++ p2p/p2p-simulation/coupled/MultiPeerGnu/testing1.bat
@@ -1,3 +1,3 @@
 cd \eclipse\workspace\P2PSimulation\p2p\p2p-simulation\coupled\MultiPeerGnu
-simu.exe -mMultiPeer5.MA -oGnOUT.out -t"2:00:00:000" > fileout.txt
+simu.exe -mMultiPeer3.MA -oGnOUT.out -t"2:00:00:000" > fileout.txt
 pause
