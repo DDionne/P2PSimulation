689f577a504269c96c50e1c9e452624ad57ed522
DDionne <djdionne@connect.carleton.ca>
2012-05-08 16:47:55 -0400

    Changed the LogEventFormatter.py so that the fileout.txt doesn't need to be modified prior to running the script, also, removed the need to copy the fileout.txt into the right directory

--- p2p/ressources/LogEventFormatter.py
+++ p2p/ressources/LogEventFormatter.py
@@ -10,21 +10,30 @@ Takes a text file with a text format like this example:
 This file is taken from the randomSurfer p2p network simulation. However lines not of the proper format must be removed
 along with any empty lines (there might be one at the end).
 """
-fi = open("fileout.txt","r")
-fileout = open("logEvents.txt","w")
+default_input = 'C:\\eclipse\\workspace'
+# root_dir will be set to my_input if they just press enter
+root_dir = raw_input("Enter your workspace directory(default: %s):\n" % default_input) or default_input
+fi = open(root_dir + "\\P2PSimulation\\p2p\\p2p-simulation\\coupled\\RSurfSimu\\fileout.txt", "r")
+
+#fi = open("fileout.txt","r")
+fileout = open("logEventsTemp.txt","w")
 f = fi.readlines()
+numLines = 0
 for line in f:
-    time = 0
-    currentLine = line.split()
-    timeList = currentLine[0].split(':')
-    hoursToMS = int(timeList[0]) * 3600000
-    minToMS = int(timeList[1]) * 60000
-    secToMS = int(timeList[2]) * 1000
-    time = hoursToMS + minToMS + secToMS + int(timeList[3])
+    if(("online" in line) or ("offline" in line) or ("publish" in line) or ("remove" in line) or ("query" in line) or ("queryhit" in line) or ("queryreachespeer" in line)):
+        time = 0
+        currentLine = line.split()
+        timeList = currentLine[0].split(':')
+        hoursToMS = int(timeList[0]) * 3600000
+        minToMS = int(timeList[1]) * 60000
+        secToMS = int(timeList[2]) * 1000
+        time = hoursToMS + minToMS + secToMS + int(timeList[3])
     if ("online" in line):
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + currentLine[2] + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + currentLine[2])
+        numLines += 1
     elif ("offline" in line):
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + currentLine[2] + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + currentLine[2])
+        numLines += 1
     elif ("connect" in line):
         toInitial = currentLine[2][:-8]
         fromPeer = currentLine[2][:-11]
@@ -33,7 +42,8 @@ for line in f:
         else:
             toFinal = toInitial
             fromPeer = "0"
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + fromPeer + ":" + str(toFinal) + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + fromPeer + ":" + str(toFinal))
+        numLines += 1
     elif ("publish" in line):
         p = currentLine[2][:-8]
         if(len(p) != 0):
@@ -47,7 +57,8 @@ for line in f:
             doc = currentLine[2]
         if p == "":
             p = "0"
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + p + ":" + doc + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + p + ":" + doc)
+        numLines += 1
     elif ("remove" in line):
         peer = currentLine[2][:-8]
         if(len(peer) != 0):
@@ -60,7 +71,8 @@ for line in f:
         else:
             peer = "0"
             doc = currentLine[2]
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc)
+        numLines += 1
     elif (" queryhit " in line):
         peer = currentLine[2][:-8]
         if(len(peer) != 0):
@@ -84,7 +96,8 @@ for line in f:
             peer = "0"
         if msg == "":
             msg = "0"
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg + "\n")
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg)
+        numLines += 1
     elif (" queryreachespeer " in line):
         peer = currentLine[2][:-8]
         if(len(peer) != 0):
@@ -100,7 +113,8 @@ for line in f:
             msg = currentLine[2][:-5]
         if peer == "":
             peer = "0"
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + peer + ":" + msg + "\n") 
+        fileout.write("\n" +  str(time) + ":" + currentLine[1] + ":" + peer + ":" + msg)
+        numLines += 1
     elif ("query" in line and "queryhit" not in line and "queryreachespeer" not in line):
         i = -4
         docs = ""
@@ -124,9 +138,19 @@ for line in f:
             msg = "0"
         if(p == ""):
             p = "0"
-        fileout.write( str(time) + ":" + currentLine[1] + ":" + p + ":" + doc + ":" + msg + "\n")
+        fileout.write("\n" + str(time) + ":" + currentLine[1] + ":" + p + ":" + doc + ":" + msg)
+        numLines += 1
+
 fi.close()
 fileout.close()
+f = open("logEventsTemp.txt","r")
+fileout = open("logEvents.txt", "w")
+fileout.write(str(numLines))
+for line in f:
+    fileout.write(line)
+f.close()
+fileout.close()
+print("Done converting the output file to the correct LogEvents format")
             
             
             
