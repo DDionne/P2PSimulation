27f32c38f4ac12369832a14f826a16a9d4bff318
DDionne <djdionne@connect.carleton.ca>
2012-06-04 13:55:15 -0400

    Fixed the LogEventFormatter.py which wasn't displaying the right documents for queryhits

--- p2p/ressources/LogEventFormatter.py
+++ p2p/ressources/LogEventFormatter.py
@@ -77,31 +77,17 @@ for line in f:
             numLines += 1
         elif (currentLine[1] == "queryhit"):
             peer = currentLine[2][:-8]
-            if(len(peer) != 0):
-                i = -8
-                msgId = ""
-                while i < -5:
-                    msgId += currentLine[2][i]
-                    i += 1
-                msg = msgId.lstrip('0')
-            elif len(peer) != 0 or len(msgId) != 0:
-                i = -4
-                docs = ""
-                while i < 0:
-                    docs += currentLine[2][i]
-                    i += 1
-                doc = docs.lstrip('0')
-
-            else:
-                doc = currentLine[2]
             if peer == "":
                 peer = "0"
+            temp = currentLine[2][:-5]
+            msg = str(int(temp)-(int(peer)*1000))
             if msg == "":
-                msg = "0"
-            if peer == "":
-                peer = "0"
+                msg = "0";
+            doc = str(int(currentLine[2]) - (int(currentLine[2][:-3])*1000))
+            if doc == "":
+                doc = "0"
             fileout.write("\n" + str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg)
-           # print(str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg)
+            #print(str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg)
             numLines += 1
         elif (currentLine[1] == "queryreachespeer"):
             peer = currentLine[2][:-8]
@@ -121,31 +107,53 @@ for line in f:
             fileout.write("\n" + str(time) + ":" + currentLine[1] + ":" + peer + ":" + msg)
            # print(str(time) + ":" + currentLine[1] + ":" + peer + ":" + msg)
             numLines += 1
-        elif (currentLine[1] == "query"):
-            i = -4
-            docs = ""
-            while i < 0:
-                docs += currentLine[2][i]
-                i += 1
-            doc = docs.lstrip('0')
-            i = -11
-            peer = ""
-            while i < -8:
-                peer += currentLine[2][i]
-                i += 1
-            p = peer.lstrip('0')
-            i = -8
-            msgId = ""
-            while i < -5:
-                msgId += currentLine[2][i]
-                i += 1
-            msg = msgId.lstrip('0')
-            if(msg == ""):
+        elif currentLine[1] == "query":
+            TTL = currentLine[2][:-11]
+            if TTL == "":
+                TTL = "0"
+            peer = str(int(currentLine[2][:-8]) - int(TTL)*1000)
+            if peer == "":
+                peer = "0"
+            msg = str((int(currentLine[2][:-5])) - (int(TTL)*1000 + int(peer))*1000)
+            if msg == "":
                 msg = "0"
-            if(p == ""):
-                p = "0"
+            temp = currentLine[2][:-3]
+            doc = str(int(currentLine[2]) - int(temp)*1000)
+            if doc == "":
+                doc = "0"
+
+            
+
+            
+##            i = -4
+##            docs = ""
+##            while i < 0:
+##                docs += currentLine[2][i]
+##                i += 1
+##            doc = docs.lstrip('0')
+##            i = -11
+##            peer = ""
+##            while i < -8:
+##                peer += currentLine[2][i]
+##                i += 1
+##            p = peer.lstrip('0')
+##            i = -8
+##            msgId = ""
+##            while i < -5:
+##                msgId += currentLine[2][i]
+##                i += 1
+##            msg = msgId.lstrip('0')
+##            if(msg == ""):
+##                msg = "0"
+##            if(p == ""):
+##                p = "0"
+
+
+
+
+                
             fileout.write("\n" + str(time) + ":" + currentLine[1] + ":" + p + ":" + doc + ":" + msg)
-           # print(str(time) + ":" + currentLine[1] + ":" + p + ":" + doc + ":" + msg)
+            #print(str(time) + ":" + currentLine[1] + ":" + peer + ":" + doc + ":" + msg)
             numLines += 1
 
 fi.close()
